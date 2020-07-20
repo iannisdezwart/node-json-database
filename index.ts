d4dd743b8526b573f3ffa07d5af60ab1024ab264
iannisdezwart <iannis__@hotmail.com>
2020-07-20 17:29:26 +0200

    some types and interfaces are now exported

--- index.ts
+++ index.ts
@@ -1,18 +1,18 @@
 import * as fs from 'fs'
 import * as chalk from 'chalk'
 
-interface DB {
+export interface DB {
 	tables: {
 		[tableName: string]: DB_Table
 	}
 }
 
-interface DB_Table {
+export interface DB_Table {
 	cols: DB_Table_Col[]
 	rows: DB_Table_Row[]
 }
 
-interface DB_Table_Col {
+export interface DB_Table_Col {
 	name: string
 	dataType: DataType
 	constraints?: Constraint[]
@@ -24,21 +24,21 @@ interface DB_Table_Col {
 	}
 }
 
-interface Link {
+export interface Link {
 	table: string
 	column: string
 }
 
-type DB_Table_Row = any[]
+export type DB_Table_Row = any[]
 
-interface DB_Table_Row_Formatted {
+export interface DB_Table_Row_Formatted {
 	[colName: string]: any
 }
 
-type DataType = 'Binary' | 'Hex' | 'Bit' | 'Int' | 'Float' | 'DateTime' | 'String' | 'Char' | 'JSON' | 'Boolean'
-type Constraint = 'primaryKey' | 'autoIncrement' | 'notNull' | 'unique'
+export type DataType = 'Binary' | 'Hex' | 'Bit' | 'Int' | 'Float' | 'DateTime' | 'String' | 'Char' | 'JSON' | 'Boolean'
+export type Constraint = 'primaryKey' | 'autoIncrement' | 'notNull' | 'unique'
 
-type RowFilterFunction = (row: DB_Table_Row_Formatted) => boolean
+export type RowFilterFunction = (row: DB_Table_Row_Formatted) => boolean
 
 export class Table {
 	cols: DB_Table_Col[]
